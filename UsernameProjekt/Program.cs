5502d3c86a0d4a9e0cc20e7aa30f9c12b03e39bf
Spektra <bergman.william@elev.ga.ntig.se>
2023-05-15 13:21:49 +0200

    first commit

---
+++ UsernameProjekt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+class Program 
+{
+    static void Main(string[] args) 
+    {
+        Console.Write("Skriv in ditt username: ");
+        string username = Console.ReadLine();
+
+        if (username.Length < 2 || username.Length > 20) 
+        {
+            Console.WriteLine("Username får endast vara mellan 2 och 20 bokstäver långt.");
+        }
+
+        else if (!Regex.IsMatch(username, "[a-zA-Z]^+$"))
+        {
+            Console.WriteLine("Username får endast innehålla bokstäver.");
+        }
+
+        else 
+        {
+            Console.WriteLine("Username är godkänt.");
+        }
+
+        Console.ReadLine();
+    }
+}
